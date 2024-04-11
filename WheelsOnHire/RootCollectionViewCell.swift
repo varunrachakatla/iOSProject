740af2f507cbb4099fbf1d5446a0ab03cd65fce4
varunrachakatla <vrachakatla@us.maxnerva.com>
2024-04-11 16:44:21 -0500

    Milestone - 02 Second Commit

---
+++ WheelsOnHire/RootCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RootCollectionViewCell.swift
+//  WheelsOnHire
+//
+//  Created by Varun Rachakatla on 11/04/24.
+//
+
+import UIKit
+
+class RootCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ WheelsOnHire/VehicleCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  VehicleCollectionViewCell.swift
+//  WheelsOnHire
+//
+//  Created by Varun Rachakatla on 11/04/24.
+//
+
+import UIKit
+
+class VehicleCollectionViewCell: UICollectionViewCell {
+    
+}
